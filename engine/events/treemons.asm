166e5b647d869f8d38e640375212c47de678935c
vulcandth <vulcandth@gmail.com>
2022-06-14 22:57:44 -0500

    Update remaining wild-Pokémon-related tables to contain 16-bit IDs
    Fix Fast Ball code doesn't work
    
    Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

--- engine/events/treemons.asm
+++ engine/events/treemons.asm
@@ -158,12 +158,15 @@ GetTreeMon:
 	cp 8
 	jr nc, NoTreeMon
 	jr .skip
+.loop
+	inc hl
+	inc hl
+	inc hl
 .skip
 	ld a, [hli]
-	cp -1
-	jr nz, .skip
-	call SelectTreeMon
-	ret
+	inc a
+	jr nz, .loop
+	; fallthrough
 
 SelectTreeMon:
 ; Read a TreeMons table and pick one monster at random.
@@ -176,6 +179,7 @@ SelectTreeMon:
 	inc hl
 	inc hl
 	inc hl
+	inc hl
 	jr .loop
 
 .ok
@@ -184,9 +188,12 @@ SelectTreeMon:
 	jr z, NoTreeMon
 
 	ld a, [hli]
-	ld [wTempWildMonSpecies], a
-	ld a, [hl]
 	ld [wCurPartyLevel], a
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	call GetPokemonIDFromIndex
+	ld [wTempWildMonSpecies], a
 	scf
 	ret
 
