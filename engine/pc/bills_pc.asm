b585889c159a2bf6f35e2aaa673e97e4c22a93e9
vulcandth <vulcandth@gmail.com>
2024-01-13 22:17:44 -0600

    Fix Non-Full Boxes > 1 in Special_CurBoxFullCheck (Rangi42/polishedcrystal#766)

--- engine/pc/bills_pc.asm
+++ engine/pc/bills_pc.asm
@@ -1479,6 +1479,9 @@ Special_CurBoxFullCheck:
 ; Returns [wScriptVar] = zero if wBufferMonBox == wCurBox
 ; Returns [wScriptVar] = nonzero if wBufferMonBox != wCurBox
 	call CurBoxFullCheck
+	jr nz, .not_equal
+	xor a
+.not_equal
 	ld [wScriptVar], a
 	ret
 
